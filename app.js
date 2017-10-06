ef0bc3cbcbfc6389ca51a642f66ccd30610626fa
TDLAM\TDLAM123 <tdlam123@gmail.com>
2017-10-06 19:43:24 +0700

    cập nhật: giản lược các phần không cần thiết và demo lưu lại access_token để request lần sau Khi load lại trang, sẽ mặc định đăng xuất

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
-var login = require('./routes/login');
 
 var app = express();
 
@@ -25,8 +23,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
-app.use('/login', login);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
